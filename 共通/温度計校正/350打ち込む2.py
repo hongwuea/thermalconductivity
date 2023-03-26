fbf86a9c94f12a8933ba76363998564a22a36b87
G118computername <G118computer>
2023-03-26 20:12:04 +0900

    G118_11_0326

--- 共通/温度計校正/350打ち込む2.py
+++ 共通/温度計校正/350打ち込む2.py
@@ -12,40 +12,41 @@ def 读取csv(文件名, 是否去掉首行=False):
             列表.append(float(数据))
 
     f = open(文件名, encoding='utf-8')
-    数据表 = [[] for _ in range(len(f.readline().strip().split(',')))]
+    数据表 = [[] for _ in range(len(f.readline().strip().split('\t')))]
     f.seek(0, 0)
     if 是否去掉首行:
         print(f.readline())
     for 行 in f:
-        list(map(lambda x, y: 添加(x, y), 数据表, 行.strip().split(',')))
+        list(map(lambda x, y: 添加(x, y), 数据表, 行.strip().split('\t')))
     数据表 = list(map(lambda x: np.array(x[0::1]), 数据表))
     f.close()
     return 数据表
 
 
-温度表 = 读取csv('真鍋cernox.csv', True)
+温度表 = 读取csv('R103.csv', True)
+# 温度表 = 温度表[1::]
 print(温度表)
 
 
 # 必ずLOG入力、さもなきゃ温度計で選択できない
-def 曲線情報打ち込む(番号=22, 名前='manabe', シリアル番号='manabeXcernox', 様式=4, 上限温度=400, 温度係数=1, ):
+def 曲線情報打ち込む(番号=21, 名前='R103', シリアル番号='R103R103', 様式=4, 上限温度=400, 温度係数=1, ):
     Ls350_1.Ls3.write(f'CRVHDR {番号},{名前},{シリアル番号},{様式},{上限温度},{温度係数}')
 
 
 # 6桁有効数字制限必須、科学計数法使用不可
 # 降温順にする必要がある、さもなきゃ正温度係数温度計として認識してしまう
-def 曲線データ打ち込む(番号=22):
+def 曲線データ打ち込む(番号=21):
     for i in range(len(温度表[0])):
-        print(f'{i}CRVPT {番号},{i + 1},{np.log10(温度表[2][-i - 1]):.6},{温度表[1][-i - 1]:.6}')
-        Ls350_1.Ls3.write(f'CRVPT {番号},{i + 1},{np.log10(温度表[2][-i - 1]):.6},{温度表[1][-i - 1]:.6}')
+        print(f'{i}CRVPT {番号},{i + 1},{np.log10(温度表[1][-i - 1]):.6},{温度表[0][-i - 1]:.6}')
+        Ls350_1.Ls3.write(f'CRVPT {番号},{i + 1},{np.log10(温度表[1][-i - 1]):.6},{温度表[0][-i - 1]:.6}')
 
 
-def 曲線削除(番号=22):
+def 曲線削除(番号=21):
     Ls350_1.Ls3.write(f'CRVDEL {番号}')
 
 
-曲線削除(21)
+曲線削除(26)
 time.sleep(1)
-曲線情報打ち込む(25)
+曲線情報打ち込む(26)
 time.sleep(3)
-曲線データ打ち込む(25)
+曲線データ打ち込む(26)
--- 圧力下熱容量測定/周波数依存性高周波.py
+++ 圧力下熱容量測定/周波数依存性高周波.py
@@ -2,20 +2,21 @@ import os
 import time
 from threading import Thread, Lock
 
+import numpy
 import numpy as np
 import pyqtgraph as pg
 
 # 同一フォルダに同名モジュールが入る場合にライブラリ導入エラーあり
 from 源.駆動 import SR850
 
-測定名 = 'G118圧力_ステップ緩和検証300K_LIA'
+測定名 = 'G118圧力_ステップ緩和検証20K_LIA'
 利得 = 1
-光源電流 = 0.5E-3  # 電流/A
+光源電流 = 0.3E-3  # 電流/A
 周波数 = 1  # 周波数/Hz
 
-频率1 = 10
+频率1 = 1
 频率2 = 1000
-点数 = 40  # 对数分布
+点数 = 60  # 对数分布
 平均次数 = 10
 SR850_1 = SR850(GPIB号=8)
 測定条件名 = f'{周波数}Hz_{光源電流 * 1e3}mA'
@@ -47,13 +48,12 @@ def 計測():
         # X表, Y表 = [], []
         time.sleep(10)
         for _ in range(平均次数):
-            X = SR850_1.读取('X')
-            Y = SR850_1.读取('Y')
+            OUT = SR850_1.读取('X') + 1j * SR850_1.读取('Y')
             time.sleep(1)
             with スレッドロック1:
                 频率表.append(频率)
-                幅值表.append(np.sqrt(X ** 2 + Y ** 2))
-                角度表.append(np.arctan(Y / X) * 180 / np.pi)
+                幅值表.append(abs(OUT))
+                角度表.append(np.angle(OUT, True))
 
 
 if __name__ == '__main__':
@@ -67,14 +67,16 @@ if __name__ == '__main__':
         右图2 = 窗口.addPlot(title="内部pt100温度計")
         右图2.setLabel(axis='left', text='R値/Volt')
         右图2.setLabel(axis='bottom', text='周波数/Hz', )
+        右图2.setLogMode(x=True, y=True)
         if 1:  # 窗口内曲线4级
             R = 右图2.plot(频率表, 幅值表, pen='b', name='R表', symbol='o', symbolBrush='b')
     if 1:  # 窗口内图3级
-        右图2 = 窗口.addPlot(title="内部pt100温度計")
-        右图2.setLabel(axis='left', text='φ値/°')
-        右图2.setLabel(axis='bottom', text='周波数/Hz', )
+        右图3 = 窗口.addPlot(title="内部pt100温度計")
+        右图3.setLabel(axis='left', text='φ値/°')
+        右图3.setLabel(axis='bottom', text='周波数/Hz', )
+        右图3.setLogMode(x=True, y=None)
         if 1:  # 窗口内曲线4级
-            φ = 右图2.plot(频率表, 角度表, pen='b', name='φ表', symbol='o', symbolBrush='b')
+            φ = 右图3.plot(频率表, 角度表, pen='b', name='φ表', symbol='o', symbolBrush='b')
 
 
     def 定时更新f():
@@ -100,8 +102,9 @@ if __name__ == '__main__':
         os.makedirs(f'結果')
     結果 = open(f'結果/{測定名}_{測定条件名}_{time.strftime("%Y年%m月%d日%H時%M分%S秒", time.localtime())}_.txt', mode='a',
               encoding='utf-8')
-    結果.write('[频率表,幅值表,角度表]\n')
-    結果.write(str([频率表, 幅值表, 角度表]))
+    結果.write('频率表\t幅值表\t角度表\n')
+    for i in range(len(频率表)):
+        結果.write(f'{频率表[i]}\t{幅值表[i]}\t{角度表[i]}\n')
     結果.flush()
     結果.close()
     print('完了しました！')
