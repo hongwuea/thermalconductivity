b7aaceed3a185b2813afb8b0ad81ab5b60af6c40
Zhangeaea <617324427@qq.com>
2023-07-16 01:02:47 +0900

    G107_2023_0716_更新了驱动和源文件

--- 源/駆動v2_0.py
+++ 源/駆動v2_0.py
@@ -10,41 +10,35 @@ GPIB锁 = Lock()
 class Ls370:
     def __init__(self, GPIB号):
         self.Ls3 = 管理器.open_resource(f'GPIB0::{GPIB号}::INSTR')
+        print(self.Ls3.query("*IDN?"))
 
-    def 读电阻(self):
-        # time.sleep(0.1)
+    def 读电阻(self, 通道=0):
         with GPIB锁:
-            return float(self.Ls3.query("RDGR?"))
+            return float(self.Ls3.query("RDGR?").strip('#'))
 
-    def 读温度(self):
-        # time.sleep(0.1)
+    def 读温度(self, 通道=0):
         with GPIB锁:
             return float(self.Ls3.query(f"KRDG?"))
 
-    def 设PID(self, P=20, I=60, D=0):
-        # time.sleep(0.1)
+    def 设PID(self, P=20, I=60, D=0, 通道=0):
         with GPIB锁:
             self.Ls3.write(f"PID {P}, {I}, {D}")
 
-    def 读加热(self):
-        # time.sleep(0.1)
+    def 读加热(self, 通道=0):
         with GPIB锁:
             return float(self.Ls3.query(f"HTR?"))
 
-    def 读加热量程(self):
-        # time.sleep(0.1)
+    def 读加热量程(self, 通道=0):
         with GPIB锁:
             return int(self.Ls3.query(f"HTRRNG?"))
 
-    def 设加热量程(self, 量程=0):
-        # time.sleep(0.1)
+    def 设加热量程(self, 量程=0, 通道=0):
         with GPIB锁:
             self.Ls3.write(f"HTRRNG {量程}")
 
-    def 设温度(self, 值):  # 这里注意用4.1E1这种科学计数法会让340不识别，默认的17位浮点数即可
-        # time.sleep(0.1)
+    def 设温度(self, 值, 通道=0):  # 这里注意用4.1E1这种科学计数法会让340不识别，默认的17位浮点数即可
         with GPIB锁:
-            self.Ls3.write(f"SETP {值}")
+            self.Ls3.write(f"SETP {值:.6}")
 
 
 class Ls350:
@@ -52,54 +46,47 @@ class Ls350:
         self.Ls3 = 管理器.open_resource(f'GPIB0::{GPIB号}::INSTR')
 
     def 读电阻(self, 通道='B'):
-        time.sleep(0.1)
         with GPIB锁:
-            return float(self.Ls3.query(f"SRDG?{通道}"))
+            return float(self.Ls3.query(f"SRDG? {通道}"))
 
     def 读温度(self, 通道='B'):
-        # time.sleep(0.1)
         with GPIB锁:
-            return float(self.Ls3.query(f"KRDG?{通道}"))
+            return float(self.Ls3.query(f"KRDG? {通道}"))
 
     def 读加热(self, 通道=2):
-        time.sleep(0.1)
         with GPIB锁:
-            return float(self.Ls3.query(f"HTR?{通道}"))
+            return float(self.Ls3.query(f"HTR? {通道}"))
 
     def 读加热量程(self, 通道=2):
-        time.sleep(0.1)
         with GPIB锁:
-            return int(self.Ls3.query(f"RANGE?{通道}"))
+            return int(self.Ls3.query(f"RANGE? {通道}"))
 
     def 设加热量程(self, 通道=2, 量程=0):
-        time.sleep(0.1)
         with GPIB锁:
-            self.Ls3.write(f"RANGE{通道},{量程}")
+            self.Ls3.write(f"RANGE {通道},{量程}")
 
     def 设温度(self, 值, 通道=2):  # 这里注意用4.1E1这种科学计数法会让340不识别，默认的17位浮点数即可
-        time.sleep(0.1)
         with GPIB锁:
-            self.Ls3.write(f"SETP{通道},{值}")
+            self.Ls3.write(f"SETP {通道},{值:.6}")
 
     def 设PID(self, P, I=60, D=0, 通道=2):
-        time.sleep(0.1)
         with GPIB锁:
-            self.Ls3.write(f"PID,{通道},{P},{I},{D}")
+            self.Ls3.write(f"PID, {通道}, {P}, {I}, {D}")
 
     def 扫引控温(self, 目标温度=250, 扫引速度K每min=0.1, 加热=0):
         with GPIB锁:
             time.sleep(0.1)
             当前温度 = float(self.Ls3.query(f"KRDG? B"))
             time.sleep(0.1)
-            self.Ls3.write(f'RAMP 2,0,0.1')
+            self.Ls3.write(f'RAMP 2, 0, 0.1')
             time.sleep(0.1)
-            self.Ls3.write(f'SETP 2,{当前温度:.5}')
+            self.Ls3.write(f'SETP 2, {当前温度:.5}')
             time.sleep(0.1)
-            self.Ls3.write(f'RANGE 2,{int(加热)}')
+            self.Ls3.write(f'RANGE 2, {int(加热)}')
             time.sleep(0.1)
-            self.Ls3.write(f'RAMP 2,1,{扫引速度K每min}')
+            self.Ls3.write(f'RAMP 2, 1, {扫引速度K每min}')
             time.sleep(0.1)
-            self.Ls3.write(f'SETP 2,{目标温度}')
+            self.Ls3.write(f'SETP 2, {目标温度}')
 
 
 class Ls340:
@@ -107,42 +94,34 @@ class Ls340:
         self.Ls3 = 管理器.open_resource(f'GPIB0::{GPIB号}::INSTR')
 
     def 读电阻(self, 通道='B'):
-        time.sleep(0.1)
         with GPIB锁:
             return float(self.Ls3.query(f"SRDG? {通道}"))
 
     def 读温度(self, 通道='B'):
-        time.sleep(0.1)
         with GPIB锁:
             return float(self.Ls3.query(f"KRDG? {通道}"))
 
     def 读加热(self, 通道=1):
-        time.sleep(0.1)
         with GPIB锁:
             return float(self.Ls3.query(f"HTR? {通道}"))
 
     def 读加热量程(self, 通道=1):
-        time.sleep(0.1)
         with GPIB锁:
             return int(self.Ls3.query(f"RANGE? {通道}"))
 
     def 设加热量程(self, 通道=1, 量程=0):
-        time.sleep(0.1)
         with GPIB锁:
-            self.Ls3.write(f"RANGE {通道}, {量程}")
+            self.Ls3.write(f"RANGE {量程}")
 
     def 设温度(self, 值, 通道=1):  # 这里注意用4.1E1这种科学计数法会让340不识别，默认的17位浮点数即可
-        time.sleep(0.1)
         with GPIB锁:
             self.Ls3.write(f"SETP {通道}, {值}")
 
     def 设PID(self, P, I=60, D=0, 通道=1):
-        time.sleep(0.1)
         with GPIB锁:
             self.Ls3.write(f"PID {通道}, {P}, {I}, {D}")
 
     def 扫引控温(self, 目标温度=50, 扫引速度K每min=1, 加热=3):
-        time.sleep(0.1)
         with GPIB锁:
             当前温度 = float(self.Ls3.query(f"KRDG? B"))
             self.Ls3.write(f'RAMP 1, 0, {扫引速度K每min}')
@@ -160,11 +139,6 @@ class K2182:
         with GPIB锁:
             return float(self.K2.query(":DATA:FRESH?"))
 
-    def 读电压t(self):
-        with GPIB锁:
-            t = time.time()
-            return float(self.K2.query(":DATA:FRESH?")), t
-
     def 触发切换(self, 开关):
         with GPIB锁:
             self.K2.write(f':initiate:continuous {开关}')
@@ -180,7 +154,13 @@ class K2182:
         with GPIB锁:
             结果 = float(self.K2.query(":DATA:FRESH?"))
             self.K2.write(':SENS:CHAN 1')
+        time.sleep(0.5)
         return 结果
+
+    def 读电压t(self):
+        with GPIB锁:
+            t = time.time()
+            return float(self.K2.query(":DATA:FRESH?")), t
 
 
 class K2400:
@@ -198,11 +178,7 @@ class K2001:
 
     def 读数据(self):
         with GPIB锁:
-            return float(self.K2.query(":FETCh?").split("N")[0])
-
-    def 九位读数(self):
-        self.K2.write(':VOLT:DC:DIG 9')
-        self.K2.write(':VOLT:DC:NPLC MAX')
+            return float(self.K2.query("FETCh?").split("N")[0])
 
 
 class K182:
@@ -263,12 +239,7 @@ class Y7651:
 
 
 class SR850:
-    字典 = {
-        'X': 1,
-        'Y': 2,
-        'R': 3,
-        'θ': 4,
-    }
+    字典 = {'X': 1, 'Y': 2, 'R': 3, 'θ': 4, }
 
     def __init__(self, GPIB号):
         self.SR8 = 管理器.open_resource(f'GPIB0::{GPIB号}::INSTR')
@@ -280,9 +251,3 @@ class SR850:
     def 设频率(self, 频率):
         with GPIB锁:
             self.SR8.write(f"FREQ {频率}")
-
-
-if __name__ == '__main__':
-    print(管理器.list_resources())
-    計測器 = 管理器.open_resource(f'GPIB0::13::INSTR')
-    print(計測器.query("*IDN?"))
